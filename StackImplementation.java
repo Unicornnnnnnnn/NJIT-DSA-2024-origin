59a51ef3745fbe35ad25baa1ef129daecb0d0017
Unicornnnnnnnn <2724471614@qq.com>
2024-03-23 23:11:47 +0800

    Exercise

---
+++ StackImplementation.java
@@ -0,0 +1,150 @@
+package oy.tol.tra;
+
+/**
+ * An implementation of the StackInterface.
+ * <p>
+ * TODO: Students, implement this so that the tests pass.
+ * 
+ * Note that you need to implement construtor(s) for your concrete StackImplementation, which
+ * allocates the internal Object array for the Stack:
+ * - a default constructor, calling the StackImplementation(int size) with value of 10.
+ * - StackImplementation(int size), which allocates an array of Object's with size.
+ *  -- remember to maintain the capacity and/or currentIndex when the stack is manipulated.
+ */
+public class StackImplementation<E> implements StackInterface<E> {
+
+   private Object [] itemArray;
+   private int capacity;
+   private int currentIndex = -1;
+   private static final int DEFAULT_STACK_SIZE = 10;
+   private int top=-1;
+
+   /**
+    * Allocates a stack with a default capacity.
+    * @throws StackAllocationException
+    */
+   public StackImplementation() throws StackAllocationException {
+      // TODO: call the constructor with size parameter with default size of 10.
+        this(DEFAULT_STACK_SIZE);
+   }
+   
+   /** TODO: Implement so that
+    * - if the size is less than 2, throw StackAllocationException
+    * - if the allocation of the array throws with Java exception,
+    *   throw StackAllocationException.
+    * @param capacity The capacity of the stack.
+    * @throws StackAllocationException If cannot allocate room for the internal array.
+    */
+   public StackImplementation(int capacity) throws StackAllocationException {
+      this.capacity=capacity;
+      
+     
+      if (capacity < 2) {
+         throw new StackAllocationException("Capacity must be at least 2.");
+     }
+     
+     try{
+         itemArray = new Object[capacity];
+     }
+     catch(Exception e)
+     {
+      throw new StackAllocationException("Cannot allocate room for the internal array.");
+     }
+   }
+         
+   
+
+   @Override
+   public int capacity() {
+      // TODO: Implement this
+      return capacity;
+   }
+
+   @Override
+   public void push(E element) throws StackAllocationException, NullPointerException {
+      // TODO: Implement this
+         if(element==null)
+         {
+            throw new NullPointerException("Error: Cannot push null value to the stack.");
+         }
+          if(top==capacity-1)
+         {
+            expandCapacity(capacity * 2); 
+         }
+         
+            top++;
+         itemArray[top] = element;
+         currentIndex++;
+         
+              
+   }
+   private void expandCapacity(int newCapacity) {
+      Object[] newArray = new Object[newCapacity];
+      for (int i = 0; i < capacity; i++) {
+          newArray[i] = itemArray[i];
+      }
+      
+      itemArray = newArray;
+      capacity = newCapacity;
+  }
+
+   @SuppressWarnings("unchecked")
+   @Override
+   public E pop() throws StackIsEmptyException {
+      if(top==-1)
+      {
+          throw new StackIsEmptyException("Stack is empty. Cannot pop.");
+      }
+      else{
+       top--;
+       currentIndex--;
+      return (E)itemArray[top+1];
+      }
+
+   }
+
+   @SuppressWarnings("unchecked")
+   @Override
+   public E peek() throws StackIsEmptyException {
+      if(top==-1)
+      {
+         throw new StackIsEmptyException("Stack is empty. Cannot peek.");
+      }
+      else{
+      return (E)itemArray[top];
+   }
+}
+
+   @Override
+   public int size() {
+      // TODO: Implement this
+      return top+1;
+   }
+
+   @Override
+   public void clear() {
+      // TODO: Implement this
+      top=-1;
+      currentIndex=-1;
+   }
+
+   @Override
+   public boolean isEmpty() {
+      // TODO: Implement this
+     return (top==-1);
+      
+   }
+
+   @Override
+   public String toString() {
+      StringBuilder builder = new StringBuilder("[");
+      for (var index = 0; index <= currentIndex; index++) {
+         builder.append(itemArray[index].toString());
+         if (index < currentIndex) {
+            builder.append(", ");
+         }
+      }
+      builder.append("]");
+      return builder.toString();
+   }
+}
